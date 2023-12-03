e7cda65b1748edb3d85c27573236dcfe76160ef5
jill64 <intents.turrets0h@icloud.com>
2023-12-03 14:59:34 +0900

    add test

---
+++ tests/smoke.spec.ts
@@ -0,0 +1,8 @@
+import { expect, test } from '@playwright/test'
+
+test('smoke', async ({ page }) => {
+  await page.goto('/')
+  await expect(
+    page.getByRole('heading', { name: '@jill64/adapter-cloudflare' })
+  ).toBeVisible()
+})
