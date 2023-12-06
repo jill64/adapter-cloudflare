543c22b5604254d840f5caf1eea1652decf3fd89
jill64 <intents.turrets0h@icloud.com>
2023-12-06 21:42:35 +0900

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,11 +4,20 @@ const ref_name = process.env.GITHUB_REF_NAME?.replace(/\//g, '-')
   .replace('.', '-')
   .slice(0, 28)
 
-export default extendsConfig({
-  use: {
-    baseURL:
-      ref_name === 'main'
-        ? 'https://adapter-cloudflare.pages.dev'
-        : `https://${ref_name}.adapter-cloudflare.pages.dev`
-  }
-})
+export default extendsConfig(
+  process.env.CI
+    ? {
+        use: {
+          baseURL:
+            ref_name === 'main'
+              ? 'https://adapter-cloudflare.pages.dev'
+              : `https://${ref_name}.adapter-cloudflare.pages.dev`
+        }
+      }
+    : {
+        webServer: {
+          command: 'npm run preview',
+          port: 4173
+        }
+      }
+)
