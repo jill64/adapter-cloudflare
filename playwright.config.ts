5e232eb81d35470d6a0e3c4dd1f9377b72979f5d
jill64 <intents.turrets0h@icloud.com>
2023-12-08 01:32:11 +0900

    Refactor playwright.config.ts for branch preview

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,23 +1,12 @@
-import { extendsConfig } from '@jill64/playwright-config'
-
-const ref_name = process.env.GITHUB_REF_NAME?.replace(/\//g, '-')
-  .replace('.', '-')
-  .slice(0, 28)
+import {
+  branchPreview,
+  extendsConfig,
+  vitePreview
+} from '@jill64/playwright-config'
 
 export default extendsConfig(
-  process.env.CI
-    ? {
-        use: {
-          baseURL:
-            ref_name === 'main'
-              ? 'https://adapter-cloudflare.pages.dev'
-              : `https://${ref_name}.adapter-cloudflare.pages.dev`
-        }
-      }
-    : {
-        webServer: {
-          command: 'npm run preview',
-          port: 4173
-        }
-      }
+  branchPreview({
+    provider: 'cloudflare',
+    fallback: vitePreview
+  })
 )
